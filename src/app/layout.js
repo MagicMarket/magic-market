a7b654bb864b19f59d6687b91f0a97b1585c98a1
MagicMarket <randomanimation45@gmail.com>
2024-05-22 12:56:18 +0800

    add image

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Bola Bulu Magic Market",
+  title: "Magic Market",
   description: "Created by Bola Bulu",
 };
 
